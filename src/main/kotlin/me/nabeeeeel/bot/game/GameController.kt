7039486d55a954ba355d7549d5cfc4347cbda172
JakeJMattson <JakeJMattson@gmail.com>
2021-04-29 00:00:58 -0500

    Remove duplicated code in GameController

--- src/main/kotlin/me/nabeeeeel/bot/game/GameController.kt
+++ src/main/kotlin/me/nabeeeeel/bot/game/GameController.kt
@@ -5,152 +5,53 @@ import java.awt.Robot
 import java.awt.Toolkit
 import java.awt.event.InputEvent
 import java.awt.event.KeyEvent
-import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-
 class GameController {
+    private val bot = Robot()
+    private val standardDelay = 500
 
-    private val standardDelay : Int = 500
-
-    fun delayBot(bot: Robot, time: Int = standardDelay) {
-        bot.delay(time)
-    }
-
-    fun getScreenShot() : File {
+    fun getScreenShot(): File {
         val (mouseX, mouseY) = listOf(100, 100)
         val (sX, sY) = listOf(19, 72)
-        val (xOffset,yOffSet) = listOf(-2, 45)
+        val (xOffset, yOffSet) = listOf(-2, 45)
 
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
+        val mask = InputEvent.BUTTON1_DOWN_MASK
         bot.mouseMove(mouseX, mouseY)
         bot.mousePress(mask)
         bot.mouseRelease(mask)
-
-        delayBot(bot)
+        bot.delay(standardDelay)
 
         val screenSize = Toolkit.getDefaultToolkit().screenSize
         val captureRect = Rectangle(sX, sY, screenSize.width / 4 - xOffset, (screenSize.height / 3 - yOffSet))
-        val screenFullImage: BufferedImage = bot.createScreenCapture(captureRect)
+        val screenFullImage = bot.createScreenCapture(captureRect)
         val file = File("screen-capture.png")
-        val status: Boolean = ImageIO.write(screenFullImage, "png", file)
-
+        ImageIO.write(screenFullImage, "png", file)
 
         return file
     }
 
-    fun findScreen(bot: Robot, mask: Int) {
+    private fun findScreen() {
         val (mouseX, mouseY) = listOf(70, 70)
         bot.mouseMove(mouseX, mouseY)
-        bot.mousePress(mask)
-        //bot.mouseRelease(mask)
+        bot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
     }
 
-    fun resize() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-        bot.keyPress(KeyEvent.VK_2)
-        delayBot(bot, 100)
-        bot.keyRelease(KeyEvent.VK_2)
+    private fun pressKey(key: Int) {
+        findScreen()
+        bot.keyPress(key)
+        bot.delay(5)
+        bot.keyRelease(key)
     }
 
-    fun pressUp() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_W)
-        delayBot(bot, 5)
-        bot.keyRelease(KeyEvent.VK_W)
-
-    }
-
-    fun pressDown() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_S)
-        delayBot(bot, 5)
-        bot.keyRelease(KeyEvent.VK_S)
-    }
-
-    fun pressRight() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_D)
-        delayBot(bot, 5)
-        bot.keyRelease(KeyEvent.VK_D)
-    }
-
-    fun pressLeft() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_A)
-        delayBot(bot, 5)
-        bot.keyRelease(KeyEvent.VK_A)
-    }
-
-    fun pressA() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-        bot.keyPress(KeyEvent.VK_L)
-        //bot.keyRelease(KeyEvent.VK_L)
-    }
-
-    fun pressB() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_K)
-        //bot.keyRelease(KeyEvent.VK_K)
-    }
-
-    fun pressSelect() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_BACK_SPACE)
-        //bot.keyRelease(KeyEvent.VK_BACK_SPACE)
-    }
-
-    fun pressStart() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_ENTER)
-        //bot.keyRelease(KeyEvent.VK_ENTER)
-
-    }
-
-    fun pressL() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_I)
-        bot.keyRelease(KeyEvent.VK_I)
-    }
-
-    fun pressR() {
-        val bot = Robot()
-        val mask: Int = InputEvent.BUTTON1_DOWN_MASK
-        findScreen(bot, mask)
-
-        bot.keyPress(KeyEvent.VK_O)
-        bot.keyRelease(KeyEvent.VK_O)
-    }
-
-    val me = listOf("hi", "hello")
+    fun resize() = pressKey(KeyEvent.VK_2)
+    fun pressUp() = pressKey(KeyEvent.VK_W)
+    fun pressDown() = pressKey(KeyEvent.VK_S)
+    fun pressLeft() = pressKey(KeyEvent.VK_A)
+    fun pressRight() = pressKey(KeyEvent.VK_D)
+    fun pressA() = pressKey(KeyEvent.VK_L)
+    fun pressB() = pressKey(KeyEvent.VK_K)
+    fun pressSelect() = pressKey(KeyEvent.VK_BACK_SPACE)
+    fun pressStart() = pressKey(KeyEvent.VK_ENTER)
 }
