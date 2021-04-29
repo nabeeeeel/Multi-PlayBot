90320bf33d55949cd9577d7c7dedba68fec4e14b
JakeJMattson <JakeJMattson@gmail.com>
2021-04-29 00:16:14 -0500

    Remove duplicated code in ButtonCommands

--- src/main/kotlin/me/nabeeeeel/bot/commands/ButtonCommands.kt
+++ src/main/kotlin/me/nabeeeeel/bot/commands/ButtonCommands.kt
@@ -1,145 +1,83 @@
 package me.nabeeeeel.bot.commands
 
 import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.entity.ReactionEmoji
+import dev.kord.x.emoji.DiscordEmoji
 import dev.kord.x.emoji.Emojis
+import me.jakejmattson.discordkt.api.dsl.CommandEvent
+import me.jakejmattson.discordkt.api.dsl.DiscordContext
 import me.jakejmattson.discordkt.api.dsl.commands
 import me.nabeeeeel.bot.services.BooleanServices.Companion.commandInProgress
 import me.nabeeeeel.bot.services.ControllerService
 
 fun buttonCommands(controller: ControllerService) = commands("Button") {
+    suspend fun CommandEvent<*>.pressButton(reaction: DiscordEmoji, action: (ControllerService) -> Unit) {
+        commandInProgress = true
+
+        controller.resize()
+        action.invoke(controller)
+        reactWith(reaction)
+
+        channel.createMessage {
+            addFile(controller.getScreenShot().toPath())
+        }
+
+        commandInProgress = false
+    }
+
     command("Up", "U", "${Emojis.arrowUp}") {
         description = "Move Up"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressUp()
-            reactWith(Emojis.arrowUp)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.arrowUp) { it.pressUp() }
         }
     }
 
     command("Down", "D", "${Emojis.arrowDown}") {
         description = "Move Down"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressDown()
-            reactWith(Emojis.arrowDown)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.arrowDown) { it.pressDown() }
         }
     }
 
     command("Left", "L", "${Emojis.arrowLeft}") {
         description = "Move Left"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressLeft()
-            reactWith(Emojis.arrowLeft)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.arrowLeft) { it.pressLeft() }
         }
     }
 
     command("Right", "R", "${Emojis.arrowRight}") {
         description = "Move Right"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressRight()
-            reactWith(Emojis.arrowRight)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.arrowRight) { it.pressRight() }
         }
     }
 
     command("A", "${Emojis.regionalIndicatorA}") {
         description = "Press the 'A' button!"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressA()
-            reactWith(Emojis.regionalIndicatorA)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.regionalIndicatorA) { it.pressA() }
         }
     }
 
     command("B", "${Emojis.regionalIndicatorB}") {
         description = "Press the 'B' button!"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressB()
-            reactWith(Emojis.regionalIndicatorB)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.regionalIndicatorB) { it.pressB() }
         }
     }
 
     command("Start") {
         description = "Press the 'Start' button!"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressStart()
-            reactWith(Emojis.whiteCheckMark)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.whiteCheckMark) { it.pressStart() }
         }
     }
 
     command("Select") {
         description = "Press the 'Select' button!"
         execute {
-            commandInProgress = true
-            controller.resize()
-            controller.pressSelect()
-            reactWith(Emojis.whiteCheckMark)
-            val f = controller.getScreenShot()
-
-            channel.createMessage {
-                addFile(f.toPath())
-            }
-
-            commandInProgress = false
+            pressButton(Emojis.whiteCheckMark) { it.pressSelect() }
         }
     }
 }
